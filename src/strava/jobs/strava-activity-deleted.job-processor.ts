d03a46c8b38617f66fd5068bf5c6b9da3fb1ed04
Jorgen Thuden <jorgen.thuden@willapay.com>
2024-09-16 14:35:57 +0200

    chore: handle activity deletion

--- src/strava/jobs/strava-activity-deleted.job-processor.ts
+++ src/strava/jobs/strava-activity-deleted.job-processor.ts
@@ -1,15 +1,22 @@
-import { StravaService } from '../strava.service';
 import { JobProcessor, QueuedJobProcessor } from '../../job/job-processor';
 import { STRAVA_ACTIVITY_DELETED_JOB, StravaActivityDeletedJob } from './jobs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { StravaActivity } from '../entities/strava-activity.entity';
+import { Logger } from '@nestjs/common';
 
 @JobProcessor(STRAVA_ACTIVITY_DELETED_JOB)
 export class StravaActivityDeletedJobProcessor
   implements QueuedJobProcessor<StravaActivityDeletedJob>
 {
-  constructor(private stravaService: StravaService) {}
+  private readonly logger = new Logger(StravaActivityDeletedJobProcessor.name);
+  constructor(
+    @InjectRepository(StravaActivity)
+    private activityRepository: Repository<StravaActivity>,
+  ) {}
 
   async processJob(job: StravaActivityDeletedJob): Promise<void> {
-    // FIXME: delete from db
-    throw new Error('Method not implemented.');
+    this.logger.log(`Deleting activity ${job.stravaActivityId}`);
+    await this.activityRepository.delete({ stravaId: job.stravaActivityId });
   }
 }
